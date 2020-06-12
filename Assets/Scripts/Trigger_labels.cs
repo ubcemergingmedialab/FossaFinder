c5c4e854b6083822e845fe69f9a9b4c95ce6656d
SeePeaJay <seepeajay@gmail.com>
2020-06-12 16:52:39 -0700

    re-adjusted scene 10, 11, and 12; removed highlights

---
+++ Assets/Scripts/Trigger_labels.cs
@@ -0,0 +1,57 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+
+    public class Trigger_labels : MonoBehaviour
+    {
+        public VRTK_InteractableObject linkedObject;
+        public GameObject Label;
+        
+
+        
+        protected bool showlabel;
+
+        protected virtual void OnEnable()
+        {
+            showlabel = false;
+            linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+            if (linkedObject != null)
+            {
+                linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+                linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+            }
+
+          
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (linkedObject != null)
+            {
+                linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+                linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
+            }
+        }
+
+        protected virtual void Update()
+        {
+            if (showlabel)
+            {
+                Label.SetActive(true);
+            }
+            else { Label.SetActive(false); }
+        }
+
+        protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+        {
+            showlabel = true;
+            //Label.SetActive(true);
+        }
+
+        protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+        {
+            showlabel = false;
+        }
+    }
+}
