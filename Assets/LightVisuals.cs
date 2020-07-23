89c6c2ba4902543ec9b3e8f33e8a388aefe96aa0
Karen <zhoyizhen_1998@hotmail.com>
2020-07-23 09:49:15 -0700

    bloom effects and light

---
+++ Assets/LightVisuals.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightVisuals : MonoBehaviour {
+	private Dictionary<string, GameObject> availablelights;
+    // Use this for initialization
+
+    /*! Setup onEnable
+* 
+* Add Enablelights() to eventsystem when enabled
+* @see GuidedTourManager()
+*/
+
+    public void OnEnable()
+    {
+        GuidedTourManager.Setlights += Enablelights;
+
+    }
+    /*! Setup OnDisable
+    * 
+    * Remove Enablelights() to eventsystem when enabled
+    * @see GuidedTourManager()
+    */
+    public void OnDisable()
+    {
+        GuidedTourManager.Setlights -= Enablelights;
+
+    }
+    /*! Initialize list
+* 
+* Add all lights in the hierarchy to the dictionary
+*/
+    public void Start()
+    {
+        availablelights = new Dictionary<string, GameObject>();
+        foreach (Transform child in transform)
+        {
+            availablelights.Add(child.name, child.gameObject);
+            
+
+        }
+
+    }
+
+    /*! \Manage lightss on the scene
+        * only activate specific lights by the number of scene
+        * @param names Names of fissures that should be shown on the scene
+     */
+
+    public void Enablelights(string[] names)
+    {
+        foreach (KeyValuePair<string, GameObject> pair in availablelights)
+        {
+            pair.Value.SetActive(false);
+        }
+        foreach (string name in names)
+        {
+            availablelights[name].SetActive(true);
+        }
+    }
+
+}
--- Assets/Scripts/GuidedTourManager.cs
+++ Assets/Scripts/GuidedTourManager.cs
@@ -28,11 +28,13 @@ public class GuidedTourManager : MonoBehaviour {
     public delegate void DuringTransitionHandler();
     public delegate void ZoomedOutHandler();
     public delegate void SetHighlightsHandler(string[] names);
+    public delegate void SetlightsHandler(string[] names);
     public delegate void SetBoundarisHandler(string[] names);
     public static event DefaultStateHandler DefaultState;
     public static event DuringTransitionHandler DuringTransition;
     public static event ZoomedOutHandler ZoomedOut;
     public static event SetHighlightsHandler SetHighlights;
+    public static event SetlightsHandler Setlights;
     public static event SetBoundarisHandler SetBoundaries;
 
     Vector3 adjustedCameraPosition;
@@ -170,6 +172,7 @@ public class GuidedTourManager : MonoBehaviour {
             DuringTransition?.Invoke();
             SetHighlights?.Invoke(sceneDataArray[currentSceneNumber - 1].highlights);
             SetBoundaries?.Invoke(sceneDataArray[currentSceneNumber - 1].boundaries);
+            Setlights?.Invoke(sceneDataArray[currentSceneNumber - 1].lights);
         }
 
         changeButtonStatesCoroutine = StartCoroutine(ChangeButtonStatesAfterTransitionIsCompleted());
