abb185cf6bbd5c9885d1ef4472e3661e411aaab2
Dante Cerron <dacerron@gmail.com>
2021-03-02 13:54:24 -0800

    set up structure for clicking on labels

---
+++ Assets/LabelListener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelListener : MonoBehaviour {
+
+    private static LabelListener _instance;
+    public static LabelListener Instance
+    {
+        get { return _instance; }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
--- Assets/Scripts/SceneTransitionButton.cs
+++ Assets/Scripts/SceneTransitionButton.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class SceneTransitionButton : MonoBehaviour {
 
     public Material defaultColor, activeColor, disabledColor;
     public int targetScene;
+    public VRTK_InteractableObject linkedObject;
     private SceneTransitionUI manager;
 
     // Use this for initialization
@@ -17,6 +19,17 @@ public class SceneTransitionButton : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    protected virtual void OnEnable()
+    {
+        linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+            linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+        }
+    }
 
     public void SetDefaultColor()
     {
@@ -31,6 +44,17 @@ public class SceneTransitionButton : MonoBehaviour {
     public void SetDisabledColor()
     {
         GetComponent<MeshRenderer>().material = disabledColor;
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        Debug.Log("Interactable Object Used");
+        manager.ButtonClicked(this.gameObject);
+    }
+
+    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+    {
+        Debug.Log("Interacatable Object Unused");
     }
 
     void OnMouseDown()
