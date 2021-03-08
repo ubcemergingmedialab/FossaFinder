3f7d68c8f03223a8e357e37cf28afa57fcbc3d9d
Dante Cerron <dacerron@gmail.com>
2021-03-08 10:19:16 -0800

    cleaned up scripts folder

--- Assets/Scripts/Utilities/AudioUtility.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class AudioUtility
-{
-    public static bool MusicVolume { get; set; }
-    public static bool EffectVolume { get; set; }
-}
