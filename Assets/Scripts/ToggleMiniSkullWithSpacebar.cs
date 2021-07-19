b78a1b75641e17639ad4a19eccfcc1c9904d8e41
Dante Cerron <dacerron@gmail.com>
2021-07-19 09:31:18 -0700

    more animations

---
+++ Assets/Scripts/ToggleMiniSkullWithSpacebar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMiniSkullWithSpacebar : MonoBehaviour {
+
+    // Use this for initialization
+    MiniSkullManager skull;
+	void Start () {
+        skull = GetComponent<MiniSkullManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(skull != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            skull.ToggleMiniSkull();
+        }
+	}
+}
